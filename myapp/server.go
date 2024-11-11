940db75c6ee021b3dcaf7de036c377c28236e2cb
Mustafa Bolat <mooshcor@outlook.com>
2024-11-11 04:07:29 -0500

    first commit

---
+++ myapp/server.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	// serve static content
+	e.Static("/", "public")
+
+	// load html files
+	indexBlob, err := os.ReadFile("public/index.html")
+	if err != nil {
+		e.Logger.Fatal("failed to load index.html: %s", err)
+	}
+
+	e.GET("/", func(c echo.Context) error {
+		return c.HTMLBlob(http.StatusOK, indexBlob)
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
